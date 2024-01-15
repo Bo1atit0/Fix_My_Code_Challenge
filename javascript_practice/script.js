348a15860df5b6057b4b2227a55d05b87c22e364
bo1atit0 <shorungbebolatito@gmail.com>
2024-01-15 16:43:17 +0100

    print square.js

---
+++ javascript_practice/script.js
@@ -0,0 +1,8 @@
+// script.js
+
+// Get the third command-line argument (user input
+base_16 = parseInt(process.argv[2], 10)
+// parseInt(base_16, 16)
+
+// Print the user input
+console.log("base 16:", base_16);
